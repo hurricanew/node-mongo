74c94730b6c4815d6d119af503d6ba436e3aef3d
Lucas Wang <lucas@lucass-mbp-2.home>
2019-05-18 21:59:35 +1000

    signup working

--- routes.js
+++ routes.js
@@ -25,6 +25,27 @@ router.get('/signup', (req,res)=>{
   res.render('signup');
 });
 
+router.post("/signup", function(req, res, next) {
+
+  var username = req.body.username;
+  var password = req.body.password;
+
+  User.findOne({ username: username }, function(err, user) {
+
+    if (err) { return next(err); }
+    if (user) {
+      req.flash("error", "User already exists");
+      return res.redirect("/signup");
+    }
+
+    var newUser = new User({
+      username: username,
+      password: password
+    });
+    newUser.save(next);
+
+  });
+});
 
 
 
